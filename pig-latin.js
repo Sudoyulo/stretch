8998753d1c7871d96f3a8aae9d4e552964479fc2
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-22 16:58:24 -0800

    pig-latin

--- pig-latin.js
+++ pig-latin.js
@@ -1,11 +1,14 @@
 const args = process.argv;
 let list = args.slice(2);
 let latin = "";
+let solution = "";
 
 for (let i = 0; i < list.length; i++) {
   latin = "";
-  for (let j = list[i].length - 1; j > -1; j--) {
+  for (let j = list[i].length - 1; j > 0; j--) {
     latin += list[i][j];
   }
-  console.log(latin);
+  latin += list[i][0] + "ay ";
+  solution += latin;
 }
+console.log(solution.trim());
